dd14401d8501b45483ffcc57cb45f378ecfbf6c5
Yan Zhuk <yan.zhuk45@gmail.com>
2018-05-22 16:41:55 -0400

    add remove ingredient functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -36,15 +36,33 @@ class BurgerBuilder extends Component {
   }
 
   removeIngredienthandler = (type) => {
+    const oldCount = this.state.ingredients[type]
+    const updatedCount = oldCount > 0 ? oldCount - 1 : oldCount;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
 
   }
 
   render() { 
+    const disabledInfo = {
+      ...this.state.ingredients
+    };
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0
+    }
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients}/>
         <BuildControls 
-          ingredientAdded={this.addIngredientHandler}/>
+          ingredientAdded={this.addIngredientHandler}
+          ingredientRemoved={this.removeIngredienthandler}
+          disabled={disabledInfo}/>
       </Aux>
       );
   }
